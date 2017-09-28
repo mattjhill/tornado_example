9ac5a5a5144d7c1d12be6f0291889eca80f95b8b
Matthew Hill <mattjhill@users.noreply.github.com>
2017-09-28 14:18:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("Hello, from Tornado!")
 
 def make_app():
     return tornado.web.Application([
